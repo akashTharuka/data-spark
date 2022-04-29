973c5644760067b2073fbf721518678d8d3bea6d
Akash <akash_tharuka@yahoo.com>
2022-04-29 14:03:01 +0530

    moved all css files to a folder in public

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './App.css';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
